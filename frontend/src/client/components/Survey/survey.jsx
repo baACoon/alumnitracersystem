06ceef5f6a8bdfce42945e2a77472e0a4eade967
AC Palomo <114208214+baACoon@users.noreply.github.com>
2024-12-02 10:59:28 +0800

    update sa client

--- frontend/src/client/components/Survey/survey.jsx
+++ frontend/src/client/components/Survey/survey.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './surveys.css';
 import Header from '../Header/header';
 import Sidebar from '../Sidebar/sidebar';
-import Footer from '../../../admin/components/Footer/Footer';
+
 
 function Survey() {
   return (
@@ -11,7 +11,6 @@ function Survey() {
       <Header />
       <Sidebar/>
       <SurveyMainPage/>
-      <Footer />
     </div>
   );
 }
@@ -25,28 +24,28 @@ function SurveyMainPage() {
   }
 
   return (
-      <div>
-              {/* Main Content */}
-      <div className="survey-table-container">
-        <h2>Pending Surveys</h2>
-        <table className="survey-table">
-          <thead>
-            <tr>
-              <th>No. </th>
-              <th>TITLE</th>
-              <th>DATE SURVEY RECEIVED</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>testing</td>
-              <td>
-                <a onClick={goToForm}>Tracer Survey Form (2024)</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <div className='survey-body'>
+                {/* Main Content */}
+        <div className="survey-table-container">
+          <h2>Pending Surveys</h2>
+          <table className="survey-table">
+            <thead>
+              <tr>
+                <th>No. </th>
+                <th>TITLE</th>
+                <th>DATE SURVEY RECEIVED</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>testing</td>
+                <td>
+                  <a onClick={goToForm}>Tracer Survey Form (2024)</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
   )
 }
