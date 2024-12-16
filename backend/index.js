16b538627257cf0c24ece4fa77e18e42a2697d2f
gigi <angelaasuntogallo@gmail.com>
2024-12-16 17:04:45 +0800

    3000 change to 5050

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://tupalumni.com', 'https://tupalumni.com', 'http://localhost:3000'],
+  origin: ['http://tupalumni.com', 'https://tupalumni.com', 'http://localhost:5050'],
   methods: ['GET', 'POST'], // Specify allowed methods
   credentials: true,       // Include cookies if needed
 }));
