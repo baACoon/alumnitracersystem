b963bcd5a4ca9205979f20181829e4e9e9fa4514
gigi <angelaasuntogallo@gmail.com>
2024-12-16 15:51:59 +0800

    cors modified

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import { connectToDatabase } from '../backend/db/connection.js';
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+app.use(cors({
+  origin: ['https://tupalumni.com']
+}));
 // Middleware
 app.use(cors());
 app.use(express.json());
