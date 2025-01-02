2eb3c6929644f638c9fec3d3064e926281bc5857
gigi <angelaasuntogallo@gmail.com>
2025-01-02 22:38:41 +0800

    adding inde.js

---
+++ frontend/src/admin/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App"; // Adjust path as needed
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App admin={true} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
