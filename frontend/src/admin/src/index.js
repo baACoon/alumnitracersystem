8620d3824f266cc6328f12b926d9be53e8d2029b
gigi <angelaasuntogallo@gmail.com>
2025-01-03 13:03:18 +0800

    build admin

--- frontend/src/admin/src/index.js
+++ frontend/src/admin/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"; // Adjust path as needed
+import App from "./App";
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App admin={true} />
-  </React.StrictMode>,
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
   document.getElementById("root")
 );
