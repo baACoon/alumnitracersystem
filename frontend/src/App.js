1af67faa92c397b6e18fca2cf8fe1f9d3f60f7a8
gigi <angelaasuntogallo@gmail.com>
2025-01-03 13:39:13 +0800

    removing admin route kasi nasa admin folder na siya

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,16 +19,6 @@ import Profile from '../src/client/components/Profile/profile';
 import SurveyForm from '../src/client/components/SurveyForm/SurveyForm';
 import CompletedForm from '../src/client/components/CompletedForm/CompletedForm';
 
-{/* for Admin path */}
-import Dashboard from '../src/admin/components/Dashboard/Dashboard';
-import AlumniPage from '../src/admin/components/Alumni/Page/AlumniPage'
-import SurveyContent from '../src/admin/components/AdminSurvey/Content/SurveyContent'
-import EvenTabs from '../src/admin/components/Event/EventTabs';
-import Opportunities from '../src/admin/components/Opportunities/Admin-Opportunities';
-import Navbar from '../src/admin/components/Navbar';
-import Login from './admin/src/Pages/Login';
-import Register from './admin/src/Pages/Register';
-
 
 
 function App() {
@@ -52,18 +42,6 @@ function App() {
             <Route path="/Profile" element={<Profile />} />
             <Route path="/SurveyForm" element={<SurveyForm />} />
             <Route path="/CompletedForm" element={<CompletedForm />} />
-
-             {/* ADMIN paths */}
-            <Route path="/admin/" element={<Dashboard />} />
-            <Route path="/admin/Dashboard" element={<Dashboard />} />
-            <Route path="/admin/AlumniPage" element={<AlumniPage />} />
-            <Route path="/admin/SurveyContent" element={<SurveyContent />} />
-            <Route path="/admin/EventTabs" element={<EvenTabs />} />
-            <Route path="/admin/Opportunities" element={<Opportunities />} />
-            <Route path="/admin/Navbar" element={<Navbar />} />
-            <Route path="/admin/Login" element={<Login />} />
-            <Route path="/admin/Register" element={<Register />} />
-
        
           </Routes>
         </BrowserRouter>
