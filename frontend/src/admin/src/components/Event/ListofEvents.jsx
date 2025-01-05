ab2b839d542cdbe0b15f7ee6921d34cbf8d4e697
gigi <angelaasuntogallo@gmail.com>
2025-01-05 12:42:42 +0800

    modify

--- frontend/src/admin/src/components/Event/ListofEvents.jsx
+++ frontend/src/admin/src/components/Event/ListofEvents.jsx
@@ -62,7 +62,7 @@ export const ListOfEvents = ({ events }) => {
           <div key={event._id} className={styles.eventBox}>
             {event.image && (
               <img
-                src={`http://localhost:5050/uploads/${event.image}`}
+                src={`https://alumnitracersystem.onrender.com/uploads/${event.image}`}
                 alt={event.title}
                 className={styles.eventImage}
                 onClick={() => handleEventClick(event)}
