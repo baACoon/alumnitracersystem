49f998a3d879e5023551eeb251b4037c73c77aaf
gigi <angelaasuntogallo@gmail.com>
2025-01-06 11:03:13 +0800

    UPDATE

--- frontend/src/admin/src/components/Alumni/Table/EmploymentHistory.jsx
+++ frontend/src/admin/src/components/Alumni/Table/EmploymentHistory.jsx
@@ -13,19 +13,13 @@ const EmploymentHistory = ({ employmentInfo }) => {
           <thead>
             <tr>
               <th>Company Name</th>
-              <th>Position</th>
               <th>Year Started</th>
-              <th>Job Status</th>
-              <th>Work Alignment</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>{employmentInfo.company_name || 'N/A'}</td>
-              <td>{employmentInfo.position || 'N/A'}</td>
               <td>{employmentInfo.year_started || 'N/A'}</td>
-              <td>{employmentInfo.job_status || 'N/A'}</td>
-              <td>{employmentInfo.work_alignment || 'N/A'}</td>
             </tr>
           </tbody>
         </table>
