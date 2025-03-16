bde6bb655a7a8cdabceb4d68fa27525bbab991db
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-03-16 15:52:20 +0800

    loading profile

--- frontend/src/client/components/Profile/profile.jsx
+++ frontend/src/client/components/Profile/profile.jsx
@@ -125,8 +125,22 @@ function ProfilePage() {
     }
   };
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  if (loading) {
+    return (
+      <div className={styles.loadingOverlay}>
+        <div className={styles.loaderContainer}>
+          <svg viewBox="0 0 240 240" height="80" width="80" className={styles.loader}>
+            <circle strokeLinecap="round" strokeDashoffset="-330" strokeDasharray="0 660" strokeWidth="20" stroke="#000" fill="none" r="105" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringA}`}></circle>
+            <circle strokeLinecap="round" strokeDashoffset="-110" strokeDasharray="0 220" strokeWidth="20" stroke="#000" fill="none" r="35" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringB}`}></circle>
+            <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="85" className={`${styles.pl__ring} ${styles.pl__ringC}`}></circle>
+            <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="155" className={`${styles.pl__ring} ${styles.pl__ringD}`}></circle>
+          </svg>
+          <p>Loading Profile...</p>
+        </div>
+      </div>
+    );
+  }
+    if (error) return <div>Error: {error}</div>;
 
   return (
     <div className={styles.profileContainer}>
