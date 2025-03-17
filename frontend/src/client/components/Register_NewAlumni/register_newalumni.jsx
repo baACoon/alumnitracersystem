c6f2263167a06d681fb6889947bb4ef57638f941
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-03-17 11:16:54 +0800

    add loading register

--- frontend/src/client/components/Register_NewAlumni/register_newalumni.jsx
+++ frontend/src/client/components/Register_NewAlumni/register_newalumni.jsx
@@ -7,8 +7,8 @@ const Register_NewAlumni = ({ closeModal }) => {
     const [lastName, setLastName] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [generatedID, setGeneratedID] = useState(null); // To display the generated unique ID
-
+    const [generatedID, setGeneratedID] = useState(null); 
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate(); // Initialize the navigate function
 
     const handleCrossCheckSurveyFormClick = () => {
@@ -40,6 +40,8 @@ const Register_NewAlumni = ({ closeModal }) => {
             confirmPassword,
         };
 
+        setLoading(true);
+
         try {
             const response = await fetch('https://alumnitracersystem.onrender.com/record/register', {
                 method: 'POST',
@@ -64,6 +66,8 @@ const Register_NewAlumni = ({ closeModal }) => {
         } catch (error) {
             console.error('Error submitting registration:', error);
             alert('There was an error with the registration request.');
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -126,6 +130,19 @@ const Register_NewAlumni = ({ closeModal }) => {
                         </button>
                     </div>
                 )}
+                        {/* Overlay Loader */}
+                              {loading && (
+                                <div className={styles.loadingOverlay}>
+                                  <div className={styles.loaderContainer}>
+                                  <svg viewBox="0 0 240 240" height="80" width="80" className={styles.loader}>
+                                    <circle strokeLinecap="round" strokeDashoffset="-330" strokeDasharray="0 660" strokeWidth="20" stroke="#000" fill="none" r="105" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringA}`}></circle>
+                                    <circle strokeLinecap="round" strokeDashoffset="-110" strokeDasharray="0 220" strokeWidth="20" stroke="#000" fill="none" r="35" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringB}`}></circle>
+                                    <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="85" className={`${styles.pl__ring} ${styles.pl__ringC}`}></circle>
+                                    <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="155" className={`${styles.pl__ring} ${styles.pl__ringD}`}></circle>
+                                  </svg>
+                                  </div>
+                                </div>
+                              )}
             </div>
         </div>
     );
