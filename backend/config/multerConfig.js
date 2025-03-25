c8fe008f63151c9ae6fd4831e9f94824f214dddf
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-03-25 13:24:49 +0800

    pag to di gumana ayaw ko na

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -6,39 +6,39 @@ dotenv.config();
 
 // Configure Cloudinary
 cloudinaryUploader.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// Use memory storage to keep files in buffer (for Cloudinary)
-const storage = multer.memoryStorage();
-
-// Create multer instance with memory storage
-const upload = multer({ storage });
-
-const uploadToCloudinary = async (buffer, publicId) => {
-  try {
-    const result = await cloudinaryUploader.uploader.upload_stream(
-      {
-        resource_type: 'auto',
-        public_id: publicId,
-      },
-      (error, result) => {
-        if (error) {
-          console.error('Error uploading to Cloudinary', error);
-          throw error;
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  
+  // Use memory storage to keep files in buffer (for Cloudinary)
+  const storage = multer.memoryStorage();
+  
+  // Create multer instance with memory storage
+  const upload = multer({ storage });
+  
+  const uploadToCloudinary = async (buffer, publicId) => {
+    try {
+      const result = await cloudinaryUploader.uploader.upload_stream(
+        {
+          resource_type: 'auto',
+          public_id: publicId,
+        },
+        (error, result) => {
+          if (error) {
+            console.error('Error uploading to Cloudinary', error);
+            throw error;
+          }
+          return result;
         }
-        return result;
-      }
-    );
-
-    buffer.pipe(result);
-  } catch (error) {
-    console.error('Error uploading to Cloudinary', error);
-    throw error;
-  }
-};
-
+      );
+  
+      buffer.pipe(result);
+    } catch (error) {
+      console.error('Error uploading to Cloudinary', error);
+      throw error;
+    }
+  };
+  
 export default upload;
 export { uploadToCloudinary };
