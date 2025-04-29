1c186e57cb94265d4977ea4793f316af2a12828a
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-04-29 17:00:31 +0800

    modal

--- frontend/src/admin/src/components/Opportunities/Opportunity-List.jsx
+++ frontend/src/admin/src/components/Opportunities/Opportunity-List.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Opportunity-List.module.css";
+import './opplistmodal.css';
 
 export default function OpportunityList() {
     const [publishedOpportunities, setPublishedOpportunities] = useState([]);
