455a7bb0ae009539bb42c1458d339e91a186b6ea
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-04-30 15:30:30 +0800

    removed multer

--- frontend/src/admin/src/components/Event/CreateEvent.jsx
+++ frontend/src/admin/src/components/Event/CreateEvent.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./CreateEvent.module.css";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import multer from "multer";
+
 
 
 export const CreateEvent = ({ onPost, onBack }) => {
