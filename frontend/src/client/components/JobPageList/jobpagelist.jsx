5afaed1b790812826f780a3a553f549a1b358283
AC Palomo <114208214+baACoon@users.noreply.github.com>
2025-05-01 02:23:11 +0800

    profile and jobgpagelist

--- frontend/src/client/components/JobPageList/jobpagelist.jsx
+++ frontend/src/client/components/JobPageList/jobpagelist.jsx
@@ -134,14 +134,9 @@ function JobListMainPage() {
       </div>
 
       {loading ? (
-        <div className="loadingOverlay">
+          <div className="loadingOverlay">
           <div className="loaderContainer">
-            <svg viewBox="0 0 240 240" height="80" width="80" className="loader">
-              <circle strokeLinecap="round" strokeDashoffset="-330" strokeDasharray="0 660" strokeWidth="20" stroke="#000" fill="none" r="105" cy="120" cx="120" className="pl__ring pl__ringA"></circle>
-              <circle strokeLinecap="round" strokeDashoffset="-110" strokeDasharray="0 220" strokeWidth="20" stroke="#000" fill="none" r="35" cy="120" cx="120" className="pl__ring pl__ringB"></circle>
-              <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="85" className="pl__ring pl__ringC"></circle>
-              <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="155" className="pl__ring pl__ringD"></circle>
-            </svg>
+            <div className="loader"></div>
             <p>Loading...</p>
           </div>
         </div>
--- frontend/src/client/components/Profile/profile.jsx
+++ frontend/src/client/components/Profile/profile.jsx
@@ -126,15 +126,10 @@ function ProfilePage() {
   return (
     <div className={styles.profileContainer}>
       {loading ? (
-        <div className={styles.loadingOverlay}>
-          <div className={styles.loaderContainer}>
-            <svg viewBox="0 0 240 240" height="80" width="80" className={styles.loader}>
-              <circle strokeLinecap="round" strokeDashoffset="-330" strokeDasharray="0 660" strokeWidth="20" stroke="#000" fill="none" r="105" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringA}`}></circle>
-              <circle strokeLinecap="round" strokeDashoffset="-110" strokeDasharray="0 220" strokeWidth="20" stroke="#000" fill="none" r="35" cy="120" cx="120" className={`${styles.pl__ring} ${styles.pl__ringB}`}></circle>
-              <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="85" className={`${styles.pl__ring} ${styles.pl__ringC}`}></circle>
-              <circle strokeLinecap="round" strokeDasharray="0 440" strokeWidth="20" stroke="#000" fill="none" r="70" cy="120" cx="155" className={`${styles.pl__ring} ${styles.pl__ringD}`}></circle>
-            </svg>
-            <p>Loading Profile...</p>
+          <div className="loadingOverlay">
+          <div className="loaderContainer">
+            <div className="loader"></div>
+            <p>Loading...</p>
           </div>
         </div>
       ) : (
